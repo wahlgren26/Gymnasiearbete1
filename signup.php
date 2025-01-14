82657d9d1d8e65ca3ddb1efe820a9f94b69142c6
wahlgren26 <malte.svardwahlgren@elev.ga.ntig.se>
2025-01-14 14:30:59 +0100

    update112412412412

--- signup.php
+++ signup.php
@@ -8,8 +8,11 @@
     <title>Sign Up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="signup.css">
+<link rel="stylesheet" href="signup.css">
+<link rel="stylesheet" href="form-validation.css">
 </head>
+
+NÅGOT FEL MED ATT NÄR DEN BLIR DESELECTED SÅ BLIR DEN STOR OM DEN ÄR INVALID, TA BORT DETTA SEN OSV
 
 <div class="col-sm-12 text-center">
 <center>
@@ -19,24 +22,24 @@
             <p class="message">Sign up now to get started with GymLog</p>
             <div class="flex">
                 <label>
-                    <input required="" placeholder="" type="text" class="input">
+                    <input required="" type="text" class="input" pattern="[a-zA-ZåäöÅÄÖ]+" title="Firstname can only contain letters.">
                     <span>Firstname</span>
                 </label>
                 <label>
-                    <input required="" placeholder="" type="text" class="input">
-                    <span>Lastname</span>
+                    <input required="" type="text" class="input" pattern="[a-zA-ZåäöÅÄÖ]+" title="Surname can only contain letters.">
+                    <span>Surname</span>
                 </label>
             </div>
-            <label>
-                <input required="" placeholder="" type="email" class="input">
-                <span>Email</span>
+           <label>
+               <input required="" type="text" class="input" pattern="[a-zA-Z0-9]+" title="Username can only contain letters and numbers.">
+                <span>Username</span>
             </label>
             <label>
-                <input required="" placeholder="" type="password" class="input">
+                <input required="" type="password" class="input">
                 <span>Password</span>
             </label>
             <label>
-                <input required="" placeholder="" type="password" class="input">
+                <input required="" type="password" class="input">
                 <span>Confirm password</span>
             </label>
             <button class="submit">Submit</button>
